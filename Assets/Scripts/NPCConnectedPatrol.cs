e0676b88e1e5484293ace44c989437c86e94820a
sarrahhc <42785057+sarrahhc@users.noreply.github.com>
2018-12-06 21:00:23 -0500

    Part 1

---
+++ Assets/Scripts/NPCConnectedPatrol.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCConnectedPatrol : MonoBehaviour
+{
+
+    [SerializeField]
+    bool _patrolWaiting;
+
+    [SerializeField]
+    float _totalWaitTime = 3f;
+
+    [SerializeField]
+    float _switchProbability = 0.2f;
+
+    NavMeshAgent _navMeshAgent;
+    ConnectedWaypoint _currentWaypoint;
+    ConnectedWaypoint _previousWaypoint;
+
+    bool _travelling;
+    bool _waiting;
+    float _waitTimer;
+    int _waypointsVisited;
+
+    public void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+        }
+        else
+        {
+            if (_currentWaypoint == null)
+            {
+                GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+                if (allWaypoints.Length > 0)
+                {
+                    while (_currentWaypoint == null)
+                    {
+                        int random = UnityEngine.Random.Range(0, allWaypoints.Length);
+                        ConnectedWaypoint startingWaypoint = allWaypoints[random].GetComponent<ConnectedWaypoint>();
+
+                        if (startingWaypoint != null)
+                        {
+                            _currentWaypoint = startingWaypoint;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError("Failed to find any waypoints for use in the scene.");
+            }
+
+        }
+
+        SetDestination();
+    }
+
+    public void Update()
+    {
+        if (_travelling && _navMeshAgent.remainingDistance <= 1.0f)
+        {
+            _travelling = false;
+            _waypointsVisited++;
+
+            if (_patrolWaiting)
+            {
+                _waiting = true;
+                _waitTimer = 0f;
+            }
+            else
+            {
+                SetDestination();
+            }
+        }
+
+        if (_waiting)
+        {
+            _waitTimer += Time.deltaTime;
+            if (_waitTimer >= _totalWaitTime)
+            {
+                _waiting = false;
+                SetDestination();
+            }
+        }
+    }
+
+    private void SetDestination()
+    {
+        if(_waypointsVisited > 0)
+        {
+            ConnectedWaypoint nextWaypoint = _currentWaypoint.nextWaypoint(_previousWaypoint);
+            _previousWaypoint = _currentWaypoint;
+            _currentWaypoint = nextWaypoint;
+        }
+
+        Vector3 targetVector = _currentWaypoint.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+        _travelling = true;
+    }
+
+}
